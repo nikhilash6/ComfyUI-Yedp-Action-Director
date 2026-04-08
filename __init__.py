cea32026d437fb1902ee55cd87a3058c0ae6598c
yedp123 <71874235+yedp123@users.noreply.github.com>
2026-04-08 17:25:20 +0800

    Update __init__.py
    added Mocap Surgeon (experimental)

--- __init__.py
+++ __init__.py
@@ -1,13 +1,15 @@
-from .nodes import YedpActionDirector
+from .nodes import YedpActionDirector, YedpMocapSurgeon
 
 # Mappings for ComfyUI to register the node
 NODE_CLASS_MAPPINGS = {
-    "YedpActionDirector": YedpActionDirector
+    "YedpActionDirector": YedpActionDirector,
+    "YedpMocapSurgeon": YedpMocapSurgeon
 }
 
 # Human-readable display name
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "YedpActionDirector": "🎬 Yedp Action Director"
+    "YedpActionDirector": "🎬 Yedp Action Director",
+    "YedpMocapSurgeon": "⚕️ Yedp Mocap Surgeon"
 }
 
 # Web directory to be served automatically by ComfyUI
