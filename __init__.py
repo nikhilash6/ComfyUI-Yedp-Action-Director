2dda5d0d825546d3b2556ee868c2c25ef8852330
Satrayana Eldin de Pecoulas <71874235+yedp123@users.noreply.github.com>
2026-05-21 18:26:21 +0800

    Update __init__.py
    added Blockout node

--- __init__.py
+++ __init__.py
@@ -1,18 +1,21 @@
-from .nodes import YedpActionDirector, YedpMocapSurgeon
+from .nodes import YedpActionDirector, YedpMocapSurgeon, YedpBlockout
 
 # Mappings for ComfyUI to register the node
 NODE_CLASS_MAPPINGS = {
     "YedpActionDirector": YedpActionDirector,
-    "YedpMocapSurgeon": YedpMocapSurgeon
+    "YedpMocapSurgeon": YedpMocapSurgeon,
+    "YedpBlockout": YedpBlockout
 }
 
 # Human-readable display name
 NODE_DISPLAY_NAME_MAPPINGS = {
     "YedpActionDirector": "🎬 Yedp Action Director",
-    "YedpMocapSurgeon": "⚕️ Yedp Mocap Surgeon"
+    "YedpMocapSurgeon": "⚕️ Yedp Mocap Surgeon",
+    "YedpBlockout": "🧊 Yedp Blockout"
 }
 
 # Web directory to be served automatically by ComfyUI
 WEB_DIRECTORY = "./web"
 
 __all__ = ["NODE_CLASS_MAPPINGS", "NODE_DISPLAY_NAME_MAPPINGS", "WEB_DIRECTORY"]
+
